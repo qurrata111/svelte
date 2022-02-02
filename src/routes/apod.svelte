1b3ec9a88850de9ee80978bf2c77de9aa4fadfd7
yuni <qurrata111@gmail.com>
2022-02-02 21:48:43 +0700

    update styling

--- src/routes/apod.svelte
+++ src/routes/apod.svelte
@@ -63,24 +63,26 @@
 				</div>
 				<div class="p-2">
 					<div class="p-1">Masukkan tanggal</div>
-					<input class="border rounded py-1 px-2 shadow-sm" type="date" bind:value={date} max={format(new Date(), "yyyy-MM-dd")} />
-					<span class="items-center" >
-						<button on:click={handleRequest} class="p-2 items-center bg-gray-100 rounded shadow-sm hover:bg-gray-200">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="16"
-								height="16"
-								fill="currentColor"
-								class="bi bi-search-heart-fill"
-								viewBox="0 0 16 16"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M9.318 12.359A6.474 6.474 0 0 1 6.5 13 6.496 6.496 0 0 1 0 6.5a6.5 6.5 0 1 1 11.742 3.844c.04.03.078.062.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1.007 1.007 0 0 1-.1-.115h.002a6.5 6.5 0 0 1-1.027.617ZM6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018Z"
-								/>
-							</svg>
-						</button>
-					</span>
+					<div class="flex justify-center items-center">
+						<input class="border rounded py-1 px-2 shadow-sm hover:shadow" type="date" bind:value={date} max={format(new Date(), "yyyy-MM-dd")} />
+						<span class="items-center px-2" >
+							<button on:click={handleRequest} class="p-2 items-center bg-gray-100 rounded shadow-sm hover:shadow hover:bg-gray-200">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-search-heart-fill"
+									viewBox="0 0 16 16"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M9.318 12.359A6.474 6.474 0 0 1 6.5 13 6.496 6.496 0 0 1 0 6.5a6.5 6.5 0 1 1 11.742 3.844c.04.03.078.062.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1.007 1.007 0 0 1-.1-.115h.002a6.5 6.5 0 0 1-1.027.617ZM6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018Z"
+									/>
+								</svg>
+							</button>
+						</span>
+					</div>
 				</div>
 			</div>
 		</div>
